8b92ba70d62af5806c0689e29efccd77822551be
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2019-02-17 03:17:30 -0300

    Criando componente para a tag de rentablidade

---
+++ components/Profitability.js
@@ -0,0 +1,22 @@
+import { profitabilityOptions } from '../utils/ProfitabilityOptions';
+
+const Profitability = ({ value = 'high', message = '$', ...props }) => (
+  <>
+    <span {...props}>{message}</span>
+
+    <style jsx>
+      {`
+        span {
+          font-size: 13px;
+          color: ${profitabilityOptions.border[value]};
+          background-color: ${profitabilityOptions.backgroundColor[value]};
+          border-radius: 100px;
+          text-align: center;
+          padding: 1px 5px;
+        }
+      `}
+    </style>
+  </>
+);
+
+export default Profitability;
