0c80a32d40eb3cbfa8ca95d0971cb5e1651f4a17
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2019-02-19 01:27:06 -0300

    Alterando nome do arquivo

---
+++ services/Get.js
@@ -0,0 +1,25 @@
+import initFirebase from './Api';
+
+const Get = async (collection, field) => {
+  const firebase = await initFirebase();
+  const result = await new Promise((resolve, reject) => {
+    firebase
+      .collection(collection)
+      .orderBy(field, 'desc')
+      .get()
+      .then(snapshot => {
+        const data = [];
+        snapshot.forEach(doc => {
+          data.push(doc.data());
+        });
+        resolve(data);
+      })
+      .catch(() => {
+        reject([]); // eslint-disable-line prefer-promise-reject-errors
+      });
+  });
+
+  return result;
+};
+
+export default Get;
