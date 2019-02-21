271ff8a0550bb6122f0000b59aff23df3c3438e0
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2019-02-21 11:36:33 -0300

    Melhorando execeções

--- services/Get.js
+++ services/Get.js
@@ -2,22 +2,20 @@ import initFirebase from './Api';
 
 const Get = async (collection, field) => {
   const firebase = await initFirebase();
-  const result = await new Promise((resolve, reject) => {
-    firebase
-      .collection(collection)
-      .orderBy(field, 'desc')
-      .get()
-      .then(snapshot => {
-        const data = [];
-        snapshot.forEach(doc => {
-          data.push({ ...doc.data(), token: doc.id });
-        });
-        resolve(data);
-      })
-      .catch(() => {
-        reject([]); // eslint-disable-line prefer-promise-reject-errors
+  const result = await firebase
+    .collection(collection)
+    .orderBy(field, 'desc')
+    .get()
+    .then(snapshot => {
+      const data = [];
+      snapshot.forEach(doc => {
+        data.push({ ...doc.data(), token: doc.id });
       });
-  });
+      return data;
+    })
+    .catch(error => {
+      throw Error(error);
+    });
 
   return result;
 };
--- services/Put.js
+++ services/Put.js
@@ -2,23 +2,19 @@ import initFirebase from './Api';
 
 const Put = async (collection, token, values) => {
   const firebase = await initFirebase();
-
   const init = token
     ? firebase
         .collection(collection)
         .doc(token)
         .set(values)
     : firebase.collection(collection).add(values);
-
-  const result = await new Promise((resolve, reject) => {
-    init
-      .then(ref => {
-        resolve((ref && ref.id) || token);
-      })
-      .catch(() => {
-        reject(); // eslint-disable-line prefer-promise-reject-errors
-      });
-  });
+  const result = await init
+    .then(ref => {
+      return (ref && ref.id) || token;
+    })
+    .catch(error => {
+      throw Error(error);
+    });
 
   return result;
 };
