94cfdc89ce430826b26ae8945510664229171718
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2019-02-25 10:08:13 -0300

    Separando responsabilidade das chaves do banco

--- src/services/Api/index.js
+++ src/services/Api/index.js
@@ -1,19 +1,12 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
-
-const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
-};
+import configFirebase from './configFirebase';
 
 const initFirebase = () => {
   try {
-    firebase.initializeApp(config);
+    firebase.initializeApp(configFirebase);
   } catch (error) {
+    console.log(error);
     if (!/already exists/.test(error.message)) {
       console.log(`Firebase didn't initialize correctly: ${error.message}`);
     }
